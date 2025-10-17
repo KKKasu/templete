08f489124ab6f34dbba42781cd2a7f298b9506f8
KKKasu <kasusang@aa:2a:2b:73:d0:b1.home>
2025-10-17 18:02:40 -0400

    1

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -1,31 +1,43 @@
 package dogapi;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * This BreedFetcher caches fetch request results to improve performance and
- * lessen the load on the underlying data source. An implementation of BreedFetcher
- * must be provided. The number of calls to the underlying fetcher are recorded.
- *
- * If a call to getSubBreeds produces a BreedNotFoundException, then it is NOT cached
- * in this implementation. The provided tests check for this behaviour.
- *
- * The cache maps the name of a breed to its list of sub breed names.
+ * A caching wrapper for any BreedFetcher.
+ * - Caches successful results (breed -> list of sub-breeds).
+ * - Does NOT cache failed lookups (BreedNotFoundException).
+ * - Always converts breed to lowercase for cache keys.
+ * - Records how many calls have been made to the delegate fetcher.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
+    private final BreedFetcher delegate;
+    private final Map<String, List<String>> cache = new HashMap<>();
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
 
+    public CachingBreedFetcher(BreedFetcher delegate) {
+        this.delegate = delegate;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        String key = breed.toLowerCase();
+
+        if (cache.containsKey(key)) {
+            return cache.get(key);
+        }
+
+        callsMade++;
+
+        try {
+            List<String> result = delegate.getSubBreeds(breed);
+            cache.put(key, result);
+            return result;
+        } catch (BreedNotFoundException e) {
+            throw e;
+        }
     }
 
-    public int getCallsMade() {
-        return callsMade;
     }
 }
--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -5,27 +5,34 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        BreedFetcher localFetcher = new BreedFetcherForLocalTesting();
+        BreedFetcher cachingFetcher = new CachingBreedFetcher(localFetcher);
+
         String breed = "hound";
-        BreedFetcher breedFetcher = new CachingBreedFetcher(new BreedFetcherForLocalTesting());
-        int result = getNumberOfSubBreeds(breed, breedFetcher);
+        int result = getNumberOfSubBreeds(breed, cachingFetcher);
+        System.out.println(breed + " has " + result + " sub breeds");
+        breed = "cat";
+        result = getNumberOfSubBreeds(breed, cachingFetcher);
         System.out.println(breed + " has " + result + " sub breeds");
 
-        breed = "cat";
-        result = getNumberOfSubBreeds(breed, breedFetcher);
-        System.out.println(breed + " has " + result + " sub breeds");
+        if (localFetcher instanceof BreedFetcherForLocalTesting) {
+            int count = ((BreedFetcherForLocalTesting) localFetcher).getCallCount();
+            System.out.println("Underlying fetcher was called " + count + " time(s).");
+        }
     }
 
     /**
-     * Return the number of sub breeds that the given dog breed has according to the
-     * provided fetcher.
+     * Return the number of sub breeds that the given dog breed has according to the provided fetcher.
      * @param breed the name of the dog breed
      * @param breedFetcher the breedFetcher to use
-     * @return the number of sub breeds. Zero should be returned if there are no sub breeds
-     * returned by the fetcher
+     * @return the number of sub breeds; zero should be returned if the breed is not found
      */
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
-        // TODO Task 3 implement this code so that it is entirely consistent with its provided documentation.
-        // return statement included so that the starter code can compile and run.
-        return -1;
+        try {
+            List<String> subBreeds = breedFetcher.getSubBreeds(breed);
+            return subBreeds.size();
+        } catch (BreedFetcher.BreedNotFoundException e) {
+            return 0;
+        }
     }
 }
